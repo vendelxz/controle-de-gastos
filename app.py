066f0b207231a0df67f1488979309c06ce338b3a
José Wendel <167533911+vendelxz@users.noreply.github.com>
2025-11-04 13:28:55 -0300

    fix: refatorações de código

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from storage import StorageManager
 from pdf_exporter import PDFGenerator
 from validator import Validator
 from gui import AppGUI
-# 1. Importe a sua nova classe
 from report_manager import GeradorRelatoriosCategoria 
 
 
@@ -12,11 +11,7 @@ def main():
     storage = StorageManager(Path("gastos.json"))
     pdf_exporter = PDFGenerator(Path("relatorio.pdf"))
     validator = Validator()
-    
-    # 2. Crie a instância da sua nova classe
     report_manager = GeradorRelatoriosCategoria(storage)
-
-    # 3. Passe a nova instância para a GUI
     app = AppGUI(storage, pdf_exporter, validator, report_manager)
     app.run()
 
